09a9769b6b9cd21f215eeb622dc84ea718db57ee
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-11-14 15:23:00 +0200

    Added User model and ran go run github.com/99designs/gqlgen

--- server/graph/schema.resolvers.go
+++ server/graph/schema.resolvers.go
@@ -7,19 +7,24 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/nicolad/go-movie-app/db"
 	"github.com/nicolad/go-movie-app/graph/generated"
 	"github.com/nicolad/go-movie-app/graph/model"
 )
 
-func (r *mutationResolver) Like(ctx context.Context, userID string, movieID string) (*model.User, error) {
+func (r *mutationResolver) Like(ctx context.Context, userID string, movieID string) (*db.User, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
-func (r *queryResolver) User(ctx context.Context, id string) (*model.User, error) {
+func (r *queryResolver) User(ctx context.Context, id string) (*db.User, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *queryResolver) Movies(ctx context.Context, search string) ([]*model.Movie, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *userResolver) Likes(ctx context.Context, obj *db.User) ([]*model.Movie, error) {
 	panic(fmt.Errorf("not implemented"))
 }
 
@@ -29,5 +34,9 @@ func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResol
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// User returns generated.UserResolver implementation.
+func (r *Resolver) User() generated.UserResolver { return &userResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type userResolver struct{ *Resolver }
