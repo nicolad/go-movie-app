afee94e954d87e14feeaaecf49ab25cb305832df
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-11-14 15:16:44 +0200

    Small improvement.

--- server/db/db.go
+++ server/db/db.go
@@ -1,18 +1,9 @@
 package db
 
 import (
-	"fmt"
-	"os"
+	"strconv"
 	"sync"
-
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/postgres" // Used in gorm
 )
-
-
-
-var instance *gorm.DB
-
 
 type User struct {
 	ID    string
@@ -21,40 +12,64 @@ type User struct {
 	m     sync.Mutex
 }
 
-
-// Init creates connection to postgres server
-func Init() {
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	dbname := os.Getenv("DB_NAME")
-	user := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-
-	db, err := gorm.Open("postgres", fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname,
-	))
-
-	if err != nil {
-		panic(err)
-	}
-
-	db.LogMode(true)
-
-	instance = db
+var users = map[string]*User{
+	"1": &User{
+		ID:   "1",
+		Name: "Chris",
+	},
+	"2": &User{
+		ID:   "2",
+		Name: "Steph",
+	},
+	"3": &User{
+		ID:   "3",
+		Name: "Peter",
+	},
+	"4": &User{
+		ID:   "4",
+		Name: "Tas",
+	},
+	"5": &User{
+		ID:   "5",
+		Name: "Cameron",
+	},
+	"6": &User{
+		ID:   "6",
+		Name: "Tim",
+	},
 }
 
-// GetInstance returns db connection instance
-func GetInstance() *gorm.DB {
-	if instance == nil {
-		panic("Database connection has not been created.")
+var nextID = struct {
+	id int
+	m  sync.Mutex
+}{7, sync.Mutex{}}
+
+func GetUser(id string) *User {
+	u, ok := users[id]
+	if !ok {
+		return nil
 	}
-	return instance
+	return u
 }
 
-// Close ends the db connection
-func Close() {
-	if instance == nil {
-		return
+func AddUser(name string) {
+	nextID.m.Lock()
+	defer nextID.m.Unlock()
+	id := strconv.Itoa(nextID.id)
+	users[id] = &User{
+		ID:   id,
+		Name: name,
 	}
-	instance.Close()
+	nextID.id++
+}
+
+func (u *User) Like(movieID string) {
+	u.m.Lock()
+	defer u.m.Unlock()
+	for _, id := range u.Likes {
+		if movieID == id {
+			return
+		}
+	}
+	u.Likes = append(u.Likes, movieID)
 }
